2ccd2dc78f29ec84682844ba8619b3c5736576bb
kohnakagawa <tsunekou1019@gmail.com>
2017-05-09 14:46:40 +0900

    ofpで実行するための環境変数設定スクリプト

---
+++ run_cfg/ofp/export_omp2.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+## clear settings
+unset I_MPI_DEBUG
+unset I_MPI_PIN_ORDER
+unset I_MPI_PIN_DOMAIN
+unset KMP_AFFINITY
+unset I_MPI_PIN_PROCESSOR_EXCLUDE_LIST
+unset I_MPI_FABRICS_LIST
+unset I_MPI_FABRICS
+unset HFI_NO_CPUAFFINITY
+
+## verbose
+# export I_MPI_DEBUG=4
+
+## set affinity
+export I_MPI_PIN_ORDER=spread
+export I_MPI_PIN_DOMAIN=auto:compact
+export KMP_AFFINITY=granularity=fine
+
+## do not bind core 0,1
+export I_MPI_PIN_PROCESSOR_EXCLUDE_LIST=0,1,68,69,136,137,204,205
+
+## communication settings
+export I_MPI_FABRICS_LIST=tmi
+export I_MPI_FABRICS=shm:tmi
+# export HFI_NO_CPUAFFINITY=1
+
