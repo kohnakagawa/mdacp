5582076433c309463d3cfa8e093e01c5ea67ef46
kohnakagawa <tsunekou1019@gmail.com>
2017-10-11 18:37:47 +0900

    環境設定スクリプト追加

--- env/reedbush-l.sh
+++ env/reedbush-l.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
 module purge
-module load intel/16.0.4.258
-module load intel-mpi/2017.2.174
-module load gnu
-module load cuda/8.0.44
-
+module load intel/16.0.4.258 gnu cuda/8.0.44 intel-mpi/2017.2.174
---
+++ env/sekirei.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+module purge
+module intel cuda mpt
